342b3201d7cb96942bb3dbb8250d25c7a14c2769
Gedeon Oscar.M <oscargedeon50@gmail.com>
2023-02-28 15:39:25 +0200

    using_forms_for_user_input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +28,9 @@ import { ShippingComponent } from './shipping/shipping.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -14,11 +15,27 @@ export class CartComponent {
   //* Defining the items property to store the products in the cart.
   items = this.cartService.getItems();
 
+  //* FormBuilder group() method to gather the user inputs
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
   ){ 
     //* making sure that the items are added to the cart
     this.items; 
+  }
+
+  //* Define an onSubmit() method to process the form. 
+  onSubmit(): void {
+    //* Process checkout data here
+    //* The clearCart() method of the CartService is used to reset the form and clear the cart.
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
 
 }
